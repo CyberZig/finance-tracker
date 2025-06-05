4cdd030918c49b0aea2135a5bec19bf0bda161f7
David Eaton <184980009+CyberZig@users.noreply.github.com>
2025-06-05 15:03:31 +0100

    Create label.tsx

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+
+const Label = React.forwardRef<
+  HTMLLabelElement,
+  React.LabelHTMLAttributes<HTLLabelElement>
+>(({ className, ...props }, ref) => (
+  <label
+    ref={ref}
+    className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${className}`}
+    {...props}
+  />
+))
+Label.displayName = "Label"
+
+export { Label }
